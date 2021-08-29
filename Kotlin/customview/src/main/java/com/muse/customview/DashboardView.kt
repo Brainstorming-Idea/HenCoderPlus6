d1ff304662707d741b16f42fc62f604e4fad4c0b
Brainstorming-Idea <18911971839@163.com>
2021-08-29 23:01:52 +0800

    仪表盘及刻度初步绘制

--- Kotlin/customview/src/main/java/com/muse/customview/DashboardView.kt
+++ Kotlin/customview/src/main/java/com/muse/customview/DashboardView.kt
@@ -7,22 +7,27 @@ import android.view.View
 
 var ANGLE = 120f
 var DASH_WIDTH = 3.px
-var DASH_LENGTH = 5.px
+var DASH_LENGTH = 10.px
 class DashboardView(context:Context, attr:AttributeSet) : View(context, attr) {
     private var arcPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var dashPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var arcPath = Path()
     private var dashPath = Path()
+    private var dash = Path()
     init {
-        arcPaint.strokeWidth = 3.px
-        arcPaint.style = Paint.Style.STROKE
-        dashPath.addRect(0f,0f, DASH_WIDTH, DASH_LENGTH,Path.Direction.CCW)
-        dashPaint.pathEffect = PathDashPathEffect(dashPath,50.px,0f,PathDashPathEffect.Style.MORPH)
+
     }
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         arcPaint.reset()
+        arcPaint.strokeWidth = 3.px
+        arcPaint.style = Paint.Style.STROKE
         arcPath.addArc(width/2f -150.px, height/2f - 150.px, width/2f + 150.px, height/2f + 150.px, ANGLE/2f + 90,360 - ANGLE)
+        dash.addRect(0f,0f, DASH_WIDTH, DASH_LENGTH,Path.Direction.CCW)
+        val pathMeasure = PathMeasure(arcPath, false)
+
+        dashPaint.pathEffect = PathDashPathEffect(dash,pathMeasure.length/20,0f,PathDashPathEffect.Style.MORPH)
+        dashPath.addArc(width/2f -150.px, height/2f - 150.px, width/2f + 150.px, height/2f + 150.px, ANGLE/2f + 90,360 - ANGLE)
 
     }
     override fun onDraw(canvas: Canvas) {
